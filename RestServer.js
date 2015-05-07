f63952756e677b68bc5aa6fb695058b91963c1a2
Gururaj Chinthamanikar <gurucg144@gmail.com>
2015-05-07 17:34:28 -0400

    MongoClient used instead of mongoose. Aggregate schema names changed to match aggregate type.

--- RestServer.js
+++ RestServer.js
@@ -1,19 +1,19 @@
 
+var mongodb = require('mongodb');
+var express       = require('express');     
 
-var mongoose   = require('mongoose');
-var express       = require('express');        
+
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/twitter';
+
 
 var app           = express();
 
-
-var DayOfTheWeek     = require('./app/model/DayOfTheWeek');
-var HourOfTheDay     = require('./app/model/HourOfTheDay');
 
 var port = process.env.PORT || 8080;
 
 
 var router = express.Router();
-mongoose.connect('mongodb://localhost:27017/twitter'); 
 
 
 router.use(function(req, res, next) {
@@ -27,46 +27,41 @@ router.get('/', function(req, res) {
 
 
 router.route('/ALL_TWEET_BY_DAY_OF_WEEK').get(function(req, res) {
-	DayOfTheWeek.find(function(err, tw) {
-            if (err)
-                res.send(err);
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Error in connecting to database:', err);
+		} else {
+			var collection = db.collection('day_aggregate');
 
-            res.json(tw);
-        });
+			collection.find().toArray(function (err, result) {
+				if (err)
+					res.send (err);
+				else
+					res.send (result);
+			});
+		}
+	});
     });
 
 router.route('/ALL_TWEET_BY_HOUR_OF_DAY').get(function(req, res) {
-	HourOfTheDay.find(function(err, tw) {
-            if (err)
-                res.send(err);
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('Error in connecting to database:', err);
+		} else {
+			var collection = db.collection('hour_aggregate');
 
-            res.json(tw);
-        });
+			collection.find().toArray(function (err, result) {
+				if (err)
+					res.send (err);
+				else
+					res.send (result);
+			});
+		}
+	});
     });
-
 app.use('/api', router);
 
 
 app.listen(port);
-
-DayOfTheWeek.remove();
-new DayOfTheWeek ({day:'MONDAY', count:100}).save();
-new DayOfTheWeek ({day:'TUESDAY', count:50}).save();
-new DayOfTheWeek ({day:'WEDNESDAY', count:10}).save();
-new DayOfTheWeek ({day:'THURSDAY', count:10}).save();
-new DayOfTheWeek ({day:'FRIDAY', count:10}).save();
-new DayOfTheWeek ({day:'SATURDAY', count:10}).save();
-new DayOfTheWeek ({day:'SUNDAY', count:10}).save();
-
-
-HourOfTheDay.remove();
-new HourOfTheDay ({hour:1, count:100}).save();
-new HourOfTheDay ({hour:2, count:50}).save();
-new HourOfTheDay ({hour:3, count:10}).save();
-new HourOfTheDay ({hour:4, count:10}).save();
-new HourOfTheDay ({hour:5, count:10}).save();
-new HourOfTheDay ({hour:6, count:10}).save();
-new HourOfTheDay ({hour:7, count:10}).save();
-
 
 console.log('Server running on port  ' + port);
