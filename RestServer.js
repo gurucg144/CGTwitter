27e6b380696f4610766b277f5b5a5c4f538a3e3a
Gururaj Chinthamanikar <gurucg144@gmail.com>
2015-05-07 17:35:33 -0400

    Formatting.

--- RestServer.js
+++ RestServer.js
@@ -1,30 +1,24 @@
-
 var mongodb = require('mongodb');
-var express       = require('express');     
-
+var express = require('express');
 
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/twitter';
 
-
-var app           = express();
-
+var app = express();
 
 var port = process.env.PORT || 8080;
 
-
 var router = express.Router();
 
-
 router.use(function(req, res, next) {
-    next();
+	next();
 });
-
 
 router.get('/', function(req, res) {
-    res.json({ message: 'Tweet aggregate interface' });
+	res.json({
+		message : 'Tweet aggregate interface'
+	});
 });
-
 
 router.route('/ALL_TWEET_BY_DAY_OF_WEEK').get(function(req, res) {
 	MongoClient.connect(url, function(err, db) {
@@ -33,15 +27,15 @@ router.route('/ALL_TWEET_BY_DAY_OF_WEEK').get(function(req, res) {
 		} else {
 			var collection = db.collection('day_aggregate');
 
-			collection.find().toArray(function (err, result) {
+			collection.find().toArray(function(err, result) {
 				if (err)
-					res.send (err);
+					res.send(err);
 				else
-					res.send (result);
+					res.send(result);
 			});
 		}
 	});
-    });
+});
 
 router.route('/ALL_TWEET_BY_HOUR_OF_DAY').get(function(req, res) {
 	MongoClient.connect(url, function(err, db) {
@@ -50,17 +44,16 @@ router.route('/ALL_TWEET_BY_HOUR_OF_DAY').get(function(req, res) {
 		} else {
 			var collection = db.collection('hour_aggregate');
 
-			collection.find().toArray(function (err, result) {
+			collection.find().toArray(function(err, result) {
 				if (err)
-					res.send (err);
+					res.send(err);
 				else
-					res.send (result);
+					res.send(result);
 			});
 		}
 	});
-    });
+});
 app.use('/api', router);
-
 
 app.listen(port);
 
